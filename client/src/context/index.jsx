7d3917e5694f23b92235d9ca13b223a5d9965646
jatinssh <jatin7838066@gmail.com>
2024-11-03 13:38:12 +0530

    updated

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -25,8 +25,23 @@ export const StateContextProvider = ({children}) => {
             console.log("contract call failure",error);
         }
     }
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+        const parsedCampaings = campaigns.map((campaign,i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }))
+        return parsedCampaings;
+    }
+
     return (
-        <StateContext.Provider value={{address,contract,connect,createCampaign:publishCampaign}}>
+        <StateContext.Provider value={{address,contract,connect,createCampaign:publishCampaign,getCampaigns}}>
             {children}
         </StateContext.Provider>
     )
